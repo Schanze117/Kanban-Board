9fea87c4defe50a6d300a6afd8cb472a75e960cc
Schanze117 <aaronschanzenbach@gmail.com>
2025-03-27 19:17:01 -0600

    more bug fixes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
@@ -18,7 +20,7 @@ export const login = async (req: Request, res: Response) => {
     return res.status(401).json({ message: 'Invalid password' });
   }
 
-const secretKey = process.env.JWT_SECRET_Key || '';
+const secretKey = process.env.JWT_SECRET_KEY || '';
 
   // Create a JWT token
   const token = jwt.sign({ username }, secretKey, { expiresIn: '1h'});
